efccf4d2e60606c8cdc7ed4c405b83bf549a0855
winprn <minhladz001@gmail.com>
2025-07-09 08:38:26 +0700

    feat: complete landing page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { MoveRight } from "lucide-react";
+import { MoveRight, QrCode, Speaker } from "lucide-react";
 import background from "@/assets/image.svg";
 import aptos from "@/assets/aptos.png";
+import square from "@/assets/square.svg";
+import rSquare from "@/assets/right-square.svg";
 
 function App() {
 	return (
@@ -10,6 +12,7 @@ function App() {
 			{/* <TopBanner /> */}
 			{/* <Header /> */}
 			<div className="relative flex justify-center min-h-screen bg-black overflow-hidden">
+				<div className="absolute w-full h-full" />
 				<img
 					src={background}
 					alt="background"
@@ -20,6 +23,36 @@ function App() {
 					alt="Aptos Coin"
 					className="absolute z-20 left-1/2 -translate-x-1/2 bottom-4 animate-bounce-subtle"
 				/>
+				<div className="absolute w-40 xl:w-72 aspect-square bottom-60 left-32">
+					<img
+						src={square}
+						alt="background square"
+						className="absolute w-full h-full z-1"
+					/>
+					<QrCode className="absolute top-0 right-0" stroke="white" size={80} />
+					<div className="relative z-10 text-[#38D1BD] text-6xl flex flex-col gap-4 pl-8 pt-8">
+						<span className="text-7xl">QR</span>
+						<span>payment</span>
+						<span className="text-white">in crypto</span>
+					</div>
+				</div>
+				<div className="absolute w-40 xl:w-72 aspect-square bottom-60 right-32">
+					<img
+						src={rSquare}
+						alt="background square"
+						className="absolute w-full h-full z-1"
+					/>
+					<Speaker
+						className="absolute bottom-0 left-0"
+						stroke="white"
+						size={80}
+					/>
+					<div className="relative z-10 text-white text-5xl flex flex-col pl-8 pt-6">
+						<span>Private.</span>
+						<span>Fast.</span>
+						<span>Paytos.</span>
+					</div>
+				</div>
 				<div className="relative flex flex-col items-center text-white text-center z-10 h-full mt-10">
 					<h2 className="text-4xl font-bold mb-4">
 						<span className="text-gradient">Privacy Payment</span> in crypto
---
+++ frontend/components/FirstTime.tsx
@@ -0,0 +1,16 @@
+export const FirstTimeMessage = () => {
+	return (
+		<Card>
+			<CardContent>
+				<p>
+					Whenever you reload the page, you need to sign a message to generate
+					keys to authenticate with backend. This step will be removed in the
+					future with zk proofs.
+				</p>
+				<button onClick={handleFirstTimeSignMessage}>
+					Sign Message to generate keys
+				</button>
+			</CardContent>
+		</Card>
+	);
+};
